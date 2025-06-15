c3284c343fb6c1972d5c7fea869291fa0f11f880
Tomas David Restrepo Osorio <MaxterDuck@gmail.com>
2025-06-14 19:44:21 -0500

    Update script.js

--- Exercises/climajs-main/script.js
+++ Exercises/climajs-main/script.js
@@ -1,64 +1,50 @@
-const apiKey = 'c900986c61a5c7dbda552049512b14dd';
+    const apiKey = 'c900986c61a5c7dbda552049512b14dd';
 
-async function getWeather() {
-  const resultDiv = document.getElementById('result');
-  const input = document.getElementById('city')
-  console.log(input.value)
-  let city = input.value
+    async function getWeather() {
+    const resultDiv = document.getElementById('result');
+    const input = document.getElementById('city')
+    console.log(input.value)
+    let city = input.value
 
-  // if (!city) {
-  //   resultDiv.innerText = 'Por favor ingresa una ciudad.';
-  //   return;
-  // }
+    // if (!city) {
+    //   resultDiv.innerText = 'Por favor ingresa una ciudad.';
+    //   return;
+    // }
 
-  try {
-    resultDiv.innerText = 'Buscando...';
+    try {
+        resultDiv.innerText = 'Buscando...';
 
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=es`
-    );
+        const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=es`
+        );
 
-    if (!response.ok) throw new Error('Ciudad no encontrada.');
+        if (!response.ok) throw new Error('Ciudad no encontrada.');
 
-    const data = await response.json();
-    const { name, main, weather} = data;
+        const data = await response.json();
+        const { name, main, weather} = data;
 
-    
+        
 
-    let sunstetoday = data.sys.sunset
-    
-    console.log(sunstetoday)
-    let hora =  Date(sunstetoday)
+        const sunrisetime = data.sys.sunrise
+        const sunsettime = data.sys.sunset
 
+        var datesunrise = new Date(sunrisetime * 1000).toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit' });
+        var datesunset = new Date(sunsettime * 1000).toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit' });
 
-    const unixTimestamp = sunstetoday;
-    const date = new Date(unixTimestamp * 1000);
-
-    console.log(date.toString());
-    console.log(date);
-
-    
+        
 
 
+        resultDiv.innerHTML = `
+        <p><strong>Ciudad:</strong> ${name}</p>
+        <p><strong>Temperatura:</strong> ${main.temp}°C</p>
+        <p><strong>Clima:</strong> ${weather[0].description}</p>
+        <p><strong>Amanecera a las:</strong> ${datesunrise}</p>
+        <p><strong>Anochecera a las:</strong> ${datesunset}</p>
+        `;
+    } 
 
-
-    
-
-
-
-    resultDiv.innerHTML = `
-      <p><strong>Ciudad:</strong> ${name}</p>
-      <p><strong>Temperatura:</strong> ${main.temp}°C</p>
-      <p><strong>Clima:</strong> ${weather[0].description}</p>
-      <p><strong>Anochecerá a las:</strong> ${date}</p>
-      
-    `;
-  } 
-  
-  catch (error) {
-    console.log(error)
-    resultDiv.innerText = error.message;
-  }
-}
-
-
+    catch (error) {
+        console.log(error)
+        resultDiv.innerText = error.message;
+    }
+    }
